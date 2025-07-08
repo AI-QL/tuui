e5a63641889cd18f3810c4fec7201327c8756796
AiQL.com <admin@aiql.com>
2025-07-08 22:26:22 +0800

    Update package dependencies to latest versions

--- src/renderer/plugins/vuetify.ts
+++ src/renderer/plugins/vuetify.ts
@@ -1,7 +1,6 @@
 import { createVuetify } from 'vuetify'
 import { ko, en, zhHans, zhHant, de, es, ja, fr, ru, pt, nl } from 'vuetify/locale'
 import { VFileUpload } from 'vuetify/labs/VFileUpload'
-import { VTreeview } from 'vuetify/labs/VTreeview'
 import { VIconBtn } from 'vuetify/labs/VIconBtn'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import 'vuetify/styles'
@@ -24,7 +23,6 @@ export default createVuetify({
   },
   components: {
     VFileUpload,
-    VTreeview,
     VIconBtn
   },
   theme: {
