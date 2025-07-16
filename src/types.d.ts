92911d0ba383ad8d216735a9666ffeb8f929a787
AiQL.com <admin@aiql.com>
2025-07-16 23:20:12 +0800

    Set moduleResolution to bundler for Vite

---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+declare module '@modelcontextprotocol/sdk/types' {
+  export * from '@modelcontextprotocol/sdk/types.d.ts'
+}
+
+declare module 'vuetify/lib/util/colors' {
+  export * from 'vuetify/lib/util/colors.js'
+}
