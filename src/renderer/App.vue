0b695c5f75686bd5e9e310f4cba05d79d40fe832
AiQL.com <admin@aiql.com>
2025-09-09 20:08:07 +0800

    Import the Inter variable font

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
 // import { DefaultLayout } from '@/renderer/components/layouts'
-import '@fontsource/inter'
+import '@fontsource/inter/100.css'
+import '@fontsource/inter/300.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/900.css'
+/* optional italic styles */
+import '@fontsource/inter/100-italic.css'
+import '@fontsource/inter/300-italic.css'
+import '@fontsource/inter/400-italic.css'
+import '@fontsource/inter/500-italic.css'
+import '@fontsource/inter/700-italic.css'
+import '@fontsource/inter/900-italic.css'
 </script>
 
 <template>
