27c2423f6024bef0ad3f69347a9e6d515af85c15
AiQL.com <admin@aiql.com>
2025-09-13 00:15:28 +0800

    Implement fixed header for popup screen

--- src/renderer/screens/PopupScreen.vue
+++ src/renderer/screens/PopupScreen.vue
@@ -26,53 +26,64 @@ const commandNotify = (item: PopupPromptsType) => {
 
 <template>
   <v-app>
-    <v-data-iterator :items="promptData" :items-per-page="-1" :search="search">
-      <template #header>
-        <v-toolbar class="py-2 px-3 drag gradient-command" density="compact">
-          <v-text-field
-            v-model="search"
-            class="no-drag"
-            density="compact"
-            placeholder="Search"
-            prepend-inner-icon="mdi-magnify"
-            variant="solo"
-            clearable
-            hide-details
-          ></v-text-field>
-        </v-toolbar>
-      </template>
-
-      <template #default="{ items }">
-        <v-list density="compact" nav>
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :value="item"
-            color="primary"
-            slim
-            :title="item.raw.title"
-            @click="commandNotify(item.raw)"
+    <v-layout>
+      <v-app-bar
+        class="py-2 px-3 drag gradient-command"
+        density="compact"
+      >
+        <v-text-field
+          v-model="search"
+          class="no-drag"
+          density="compact"
+          placeholder="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo"
+          clearable
+          hide-details
+        ></v-text-field>
+      </v-app-bar>
+      <v-main scrollable>
+        <v-sheet variant="flat" class="pt-4">
+          <v-data-iterator
+            :items="promptData"
+            :items-per-page="-1"
+            :search="search"
+            class="container"
           >
-            <template #prepend>
-              <v-icon class="mx-0" :icon="item.raw.icon || 'mdi-file-outline'"></v-icon>
+            <template #default="{ items }">
+              <v-list density="compact" nav class="container">
+                <v-list-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  :value="item"
+                  color="primary"
+                  slim
+                  :title="item.raw.title"
+                  @click="commandNotify(item.raw)"
+                >
+                  <template #prepend>
+                    <v-icon class="mx-0" :icon="item.raw.icon || 'mdi-file-outline'"></v-icon>
+                  </template>
+                </v-list-item>
+              </v-list>
             </template>
-          </v-list-item>
-        </v-list>
-      </template>
-    </v-data-iterator>
+          </v-data-iterator>
+        </v-sheet>
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
 <style>
+body::-webkit-scrollbar {
+  display: none;
+}
+
 .drag {
   app-region: drag;
 }
 .no-drag {
   app-region: no-drag;
-}
-
-::-webkit-scrollbar {
-  display: none;
 }
 
 .gradient-command {
